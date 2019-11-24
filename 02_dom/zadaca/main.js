a82416e8ba5843af18324f4f4abe4bfbe5eb9b60
Ivan Bogdan <bogdan.ivan@nsoft.com>
2019-11-24 15:53:42 +0100

    03_DOM
    This commit has updated styleguide, homework from 02_DOM, and a task from 03_DOM where by pressing an appropriate key on keyboard you will either reset value to none, or disable input completely
    
    Commit #6

--- 02_dom/zadaca/main.js
+++ 02_dom/zadaca/main.js
@@ -9,7 +9,7 @@ const imgLinks = [
   'https://www.thegamecrafter.com/overlays/smallsquareboard.png',
 ];
 
-for( var i = 0; i<5; i++ ) {
+for(var i = 0; i<5; i++) {
   const article = document.createElement('article');
 
   const h1 = document.createElement('h1');
@@ -41,6 +41,8 @@ for( var i = 0; i<5; i++ ) {
 
 // Koristim IFFY funkciju da ima vlastiti lokalni scope
 // Jer sam pri dnu opet deklarirao konstantu 'a' i izbacuje mi error
+
+/*
 (function() {
   const article = document.createElement('article');
 
@@ -69,6 +71,7 @@ for( var i = 0; i<5; i++ ) {
 
   div.insertBefore(article, div.firstElementChild);
 })();
+*/
 
 document.body.insertBefore(div, document.body.firstElementChild);
 
@@ -86,14 +89,17 @@ for (let i=0; i < div.childElementCount-1; i++) {
   }
 }
 
-// Izbacimo sve članove koji se ponavljaju:
+// Zamjenimo prvi i zadnji element:
 div.appendChild(div.replaceChild(div.lastElementChild, div.firstElementChild));
 
+// U predzadnji element ubacimo link koji otvara google.com u novom tabu:
 const a = document.createElement('a');
 a.setAttribute('href','https://www.google.com/');
+a.setAttribute('target', '_blank');
 a.innerHTML = "Google";
-
 div.lastElementChild.previousElementSibling.appendChild(a);
+
+// Sve iteme koji sadrže neparne brojeve obojimo u plavo, a sve koji sadrže parne u crveno:
 
 // U primjeru rješenja zadaće koji sam pokazivao na predavanju sam imao grešku.
 // Obojio sam svaki sljedeći element u različitu boju, umjesto da sam ih bojao relativno na vrijednost koju sadržavaju u <p></p> tagu.
@@ -111,12 +117,13 @@ for(let i=0; i<div.childElementCount; i++){
 */
 
 // Dodatni primjer rješavanja zadaće bez 'for' petlje:
-
 Array.from(div.children).forEach((item, i) => {
   const numb = item.getElementsByTagName('p')[0].innerText.split(' ')[1];
   div.children[i].style.backgroundColor = numb%2 !== 0 ? 'blue' : 'red';
 });
 
+
+// U head tag dodajmo stil s lokacije css/style.css
 const css = document.createElement('link');
 css.setAttribute('rel','stylesheet');
 css.setAttribute('type','text/css');
